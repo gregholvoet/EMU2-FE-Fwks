57c090f379ed82f77201469ffa59fe23f7c0dc0c
Grégoire Holvoet <gho@rtbf.be>
2023-11-06 13:54:05 +0100

    ajout exercice jquery

---
+++ jquery/js/script.js
@@ -0,0 +1,11 @@
+// cette ligne permet d'être sûr que le document est prêt à utiliser jQuery
+$(function() {
+    /*  
+    Quand je clique sur un élément de la navigation, 
+    le composant : 
+    1. met en avant le lien sur lequel je viens de cliquer
+    2. affiche l'image adéquate
+    3. affiche le paragraphe adéquat
+    */
+    
+});
